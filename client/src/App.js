35d865a11eae8ccd08374e98d314a434284324b0
Zelling <zelling@localhost.localdomain>
2019-11-06 10:36:52 -0300

    React/Express Authentication section done

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import setAuthToken from "./utils/setAuthToken";
+
 import Navbar from "./Components/Layout/Navbar";
 import Home from "./Components/Pages/Home";
 import About from "./Components/Pages/About";
+import Register from "./Components/Auth/Register";
+import Login from "./Components/Auth/Login";
+import Alerts from "./Components/Layout/Alerts";
+import PrivateRoute from "./Components/routing/PrivateRoute";
+
 import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/register" component={Register} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 export default App;
